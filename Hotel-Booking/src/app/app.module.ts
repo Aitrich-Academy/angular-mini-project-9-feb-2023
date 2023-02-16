d70becd1c83fc1a564f578f37ac47158bd7f1ae2
lakshmi <lakshmirkrishna7@gmail.com>
2023-02-16 14:01:16 +0530

    lak

--- Hotel-Booking/src/app/app.module.ts
+++ Hotel-Booking/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PackagesComponent } from './packages/packages.component';
+import { PackagedetailComponent } from './packagedetail/packagedetail.component';
+import { BookingdetailComponent } from './bookingdetail/bookingdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PackagesComponent,
+    PackagedetailComponent,
+    BookingdetailComponent
   ],
   imports: [
     BrowserModule,
